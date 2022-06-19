f1a625b933913e67c93da3d715887c64e4acfe9b
Sheraff <fpellet@ensc.fr>
2022-06-19 20:13:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react'
+import './styles.css'
+import { useFragment, Receptacle, Portal } from './FragmentPortal'
 
-function App() {
+export default function App() {
+  const fragment = useFragment('div')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slots fragment={fragment} />
+      <LiveComponents fragment={fragment} />
     </div>
-  );
+  )
 }
 
-export default App;
+function LiveComponents({ fragment }) {
+  const [, rerender] = useState(0)
+  const i = useRef(0).current++
+  return (
+    <Portal fragment={fragment}>
+      <button onClick={() => rerender({})}>rerender: {i}</button>
+      <Video />
+      <Timer />
+    </Portal>
+  )
+}
+
+function Slots({ fragment }) {
+  const [side, setSide] = useState(false)
+  const onClick = () => setSide((a) => !a)
+  const slotRef = useRef()
+  return (
+    <>
+      <button onClick={onClick}>
+        {!side && '←'} switch {side && '→'}
+      </button>
+      <ul className="grid">
+        <li ref={slotRef}>
+          <Receptacle
+            slot={slotRef} // into ref Node
+            fragment={side && fragment}
+            props={{ label: 'left' }}
+          />
+        </li>
+        <Receptacle
+          type="li" // as standalone Node
+          fragment={!side && fragment}
+          props={{ label: 'right' }}
+        />
+      </ul>
+    </>
+  )
+}
+
+function Video({ label }) {
+  const i = useRef(0).current++
+  return (
+    <>
+      <video width="320" height="240" controls loop muted autoPlay playsInline>
+        <source
+          src="https://www.w3docs.com/build/videos/arcnet.io(7-sec).mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div>
+        renders: {i}, parent: {label}
+      </div>
+    </>
+  )
+}
+
+function Timer({ label }) {
+  const [time, setTime] = useState(0)
+  useEffect(() => {
+    const id = setInterval(() => setTime((t) => t + 1), 1000)
+    return () => clearInterval(id)
+  }, [])
+  const i = useRef(0).current++
+  return (
+    <p>
+      {time}s since mounted, renders: {i}, parent: {label}
+    </p>
+  )
+}
---
+++ src/FragmentPortal/index.jsx
@@ -0,0 +1,68 @@
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react'
+import { createPortal } from 'react-dom'
+
+const fragments = document.createDocumentFragment()
+
+export function useFragment(type = 'div') {
+  const [fragment] = useState(() => {
+    const element = document.createElement(type)
+    fragments.appendChild(element)
+    return element
+  })
+  useEffect(
+    () => () => {
+      fragment.parentElement?.removeChild(fragment)
+    },
+    [fragment],
+  )
+  return fragment
+}
+
+export function Receptacle({ fragment, props, type = 'div', slot }) {
+  const div = useRef()
+
+  useLayoutEffect(() => {
+    if (!fragment) {
+      return undefined
+    }
+    const receptacle = slot ? slot.current : div.current
+    receptacle.appendChild(fragment)
+    return () => {
+      fragments.appendChild(fragment)
+    }
+  }, [slot, fragment])
+
+  fragment.getProps = () => props
+  useLayoutEffect(() => {
+    if (!fragment || fragment.currentProps() === props) return
+    fragment.setProps(props)
+  }, [fragment, props])
+
+  if (!fragment) return null
+
+  if (slot) return null
+
+  const Element = type
+  return <Element ref={div} />
+}
+
+export function Portal({ children, fragment }) {
+  const [extraProps, setExtraProps] = useState(fragment.getProps?.() || {})
+  fragment.setProps = setExtraProps
+  fragment.currentProps = () => extraProps
+  return createPortal(
+    Children.map(children, (child) => {
+      if (!isValidElement(child)) return child
+      return cloneElement(child, extraProps)
+    }),
+    fragment,
+  )
+}
